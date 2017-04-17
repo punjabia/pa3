9d8171866e32994f872129bc8b1b486c62ce59e3
Amol Punjabi <apunjabi@Amols-MacBook-Pro.local>
2017-04-16 23:15:17 -0400

    Finished KarkmarkarKarp?

--- Driver.java
+++ Driver.java
@@ -1,3 +1,14 @@
+import java.util.*;
+
 public class Driver {
-	
+	public static void main(String[] args) {
+		long[] list = new long[5];
+		Random rnd = new Random();
+
+		for (int i = 0; i < 5; i++) {
+			list[i] = rnd.nextInt(10);
+		}
+		KK part = new KK(list);
+		System.out.println(part.residue(part.heap));
+	}
 }
